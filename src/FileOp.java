7a1140af1d4ef31fcd32e8f52e4f9c15f55c2de1
mrducky <kakashipll@gmail.com>
2018-02-17 18:07:11 +0100

    3

--- src/FileOp.java
+++ src/FileOp.java
@@ -1,20 +1,17 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.PrintWriter;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Scanner;
+import java.io.*;
+import java.net.URL;
+import java.nio.file.Files;
+import java.util.*;
 
 public class FileOp {
-    List<String> dataList = new ArrayList<>();
+    //List<String> dataList = new ArrayList<>();
     static String fileName = "data";
-    public void FileRead(){//chyba jest ok ale nie zapisuje do tablicy a moglby
+
+    public void FileRead() {
         try {
-            Scanner in = new Scanner(new File(fileName +".txt"));
+            Scanner in = new Scanner(new File(fileName + ".txt"));
             while (in.hasNextLine()) {
                 System.out.println(in.nextLine());
-                //dataList.add(in.nextLine());
             }
             in.close();
         } catch (FileNotFoundException e) {
@@ -22,25 +19,16 @@ public class FileOp {
         }
 
     }
-    public void AddToFile(String addStrToFile){ //nie dodaje na koncu tylko nadpisuje plik
-        try {
-            PrintWriter pw = new PrintWriter(fileName +".txt");
-            pw.println(addStrToFile);
-            pw.close();
-            pw.flush();
-        }catch(FileNotFoundException e){
-            System.out.println("brak pliku");
-        }
-    }
 
-    public int countWords(String word){ //nie lapie slow wgle (test)
+    public int countWords(String word) {
         int counter = 0;
         String test;
         try {
-            Scanner in = new Scanner(new File(fileName +".txt"));
-            while (in.hasNextLine()) {
-                test = in.next();
-                if(test.equals(word)){
+            Scanner in = new Scanner(new File(fileName + ".txt"));
+            while (in.hasNext()) {
+                test = in.nextLine();
+                //System.out.println(test);
+                if (test.equals(word)) {
                     counter++;
                 }
             }
@@ -52,55 +40,169 @@ public class FileOp {
 
         return counter;
     }
-    public int countChars(char letter){ //jw
-        int counter = 0;
-        char scanner;
+
+    public int countChars(char letter) {
+        int chars = 0;
+        int ch;
         try {
-            Scanner in = new Scanner(new File(fileName +".txt"));
-            //scanner = in.next();
-            /*while(){
-
-            }*/
-
-            in.close();
-        } catch (FileNotFoundException e) {
-            System.out.println("brak pliku");
-        }
-
-
-        return counter;
-
-    }
-
-    public void deleteString(String delStr){
-        try {
-            //potrzebuje liste z pliku zeby usunac i zapisac od nowa?
-            Scanner in = new Scanner(new File(fileName +".txt"));
-            PrintWriter pw = new PrintWriter(new File(fileName +".txt"));
-            String del = in.nextLine();
-            //scanner = in.next();
-            if (!del.trim().equals(delStr)) {
-                pw.println();
+            BufferedReader reader = new BufferedReader(new FileReader(fileName + ".txt"));
+            while ((ch = reader.read()) != -1) {
+                if ((char) ch == letter) {
+                    chars++;
+                }
             }
 
-            in.close();
-        } catch (FileNotFoundException e) {
+            reader.close();
+        } catch (IOException e) {
             System.out.println("brak pliku");
         }
 
+
+        return chars;
 
     }
-    public void modify(String modStr,String modMod){ //??
-        try {
-            Scanner in = new Scanner(new File(fileName +".txt"));
-            String rep = in.nextLine();
-            //scanner = in.next();
-            rep = rep.replace(modStr,modMod);
 
-            in.close();
-        } catch (FileNotFoundException e) {
-            System.out.println("brak pliku");
+    public void deleteStringToNewFile(String delStr) {
+        File tempFile = new File(delStr + "_temp" + ".txt");
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(fileName + ".txt"));
+            BufferedWriter writer = new BufferedWriter(new FileWriter(tempFile));
+            String currentLine;
+
+            while ((currentLine = reader.readLine()) != null) {
+                // trim newline when comparing with lineToRemove
+                String trimmedLine = currentLine.trim();
+                if (trimmedLine.equals(delStr)) continue;
+                writer.write(currentLine + System.getProperty("line.separator"));
+
+            }
+            writer.close();
+            reader.close();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
         }
 
+    }
+
+    public void deleteStringToSameFile(String delStr) {
+        File mainFile = new File(fileName + ".txt");
+        File tempFile = new File(delStr + "_temp" + ".txt");
+        deleteStringToNewFile(delStr);
+        // Files.deleteIfExists(fileName);
+        mainFile.delete();
+        tempFile.renameTo(mainFile);
+
+    }
+
+    public void modify(String modStr, String modMod) { //??
+        File tempFile = new File(modStr + "_temp" + ".txt");
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(fileName + ".txt"));
+            BufferedWriter writer = new BufferedWriter(new FileWriter(tempFile));
+            String currentLine;
+
+            while ((currentLine = reader.readLine()) != null) {
+
+                String modLine = currentLine.trim();
+                if (modLine.equals(modStr)) {
+                    writer.write(modMod + System.getProperty("line.separator"));
+                } else {
+                    writer.write(currentLine + System.getProperty("line.separator"));
+                }
+
+            }
+            writer.close();
+            reader.close();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void sortAsc() {
+        try {
+            ArrayList<String> rows = new ArrayList<String>();
+            BufferedReader reader = new BufferedReader(new FileReader(fileName + ".txt"));
+
+            String s;
+            while ((s = reader.readLine()) != null)
+                rows.add(s);
+
+            Collections.sort(rows);
+
+            FileWriter writer = new FileWriter(fileName + "sortAsc" + ".txt");
+            for (String cur : rows)
+                writer.write(cur + System.getProperty("line.separator"));
+
+            reader.close();
+            writer.close();
+        } catch (IOException e) {
+            System.out.println("exception");
+        }
+    }
+
+    public void sortDsc() {
+        try {
+            ArrayList<String> rows = new ArrayList<String>();
+            BufferedReader reader = new BufferedReader(new FileReader(fileName + ".txt"));
+
+            String s;
+            while ((s = reader.readLine()) != null)
+                rows.add(s);
+
+            Collections.sort(rows, Collections.reverseOrder());
+            // Collections.reverseOrder();
+
+            FileWriter writer = new FileWriter(fileName + "sortDsc" + ".txt");
+            for (String cur : rows)
+                writer.write(cur + System.getProperty("line.separator"));
+
+            reader.close();
+            writer.close();
+        } catch (IOException e) {
+            System.out.println("exception");
+        }
+    }
+
+    public void insertInto(String what, int where) {
+        File tempFile = new File(fileName + "_insertInto" + ".txt");
+        int lineCounter = 1;
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(fileName + ".txt"));
+            BufferedWriter writer = new BufferedWriter(new FileWriter(tempFile));
+            String currentLine;
+
+            while ((currentLine = reader.readLine()) != null) {
+                if (lineCounter == where) {
+                    writer.write(what + System.getProperty("line.separator"));
+                }
+                writer.write(currentLine + System.getProperty("line.separator"));
+                lineCounter++;
+            }
+
+            writer.close();
+            reader.close();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void readURL() {
+        try {
+            int countWords = 0;
+            int countLetters = 0;
+            URL url = new URL("http://www.puzzlers.org/pub/wordlists/pocket.txt");
+            BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
+            String currentLine;
+            while ((currentLine = in.readLine()) != null) {
+                System.out.println(currentLine);
+                countWords++;
+                countLetters+=currentLine.length();
+            }
+            in.close();
+            System.out.println(countWords);
+            System.out.println(countLetters);
+        } catch (IOException e) {
+            System.out.println("exception in URL reading");
+        }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,38 +1,76 @@
-import java.io.FileNotFoundException;
-import java.io.PrintWriter;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
         String addThis = "add this";
+        String deleteThis = "Janek";
         String countThis = "Janek";
-        Scanner sc = new Scanner(System.in);
-        String check;
-        System.out.println("Zapis do pliku data.txt = 1 a do innego = 2");
-        check = sc.nextLine();
-        if(check.equals("1")){
-            FileOp.fileName = "data";
-        }else if(check.equals("2")){
-            System.out.println("Podaj nowa nazwe pliku");
-            FileOp.fileName = sc.nextLine();
-            try {
-                PrintWriter pw = new PrintWriter(FileOp.fileName+".txt");
-                pw.close();
-                pw.flush();
-                System.out.println("Utworzono plik");
-            }catch(FileNotFoundException e){
-                System.out.println("nie utworzono pliku");
-            }
-        }else{
-            System.out.println("nie ma takiego numeru");
-            return;
-        }
+        String s1 = "Asia";
+        String s2 = "Kasia";
+        char countChar = 'a';
+        int count;
         FileOp fileReader = new FileOp();
-        fileReader.FileRead();
-       // fileReader.AddToFile(addThis);
-        //fileReader.FileRead();*/
-       // fileReader.countWords(countThis);
+        int s;
+        int check1;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("podaj liczbe: ");
+        System.out.println("1: zliczenie slowa " +countThis);
+        System.out.println("2: zliczenie znaku " +countChar);
+        System.out.println("3: usuwanie ciagu z zapisem do nowego pliku "+ deleteThis);
+        System.out.println("4: zamiana ciagu na inny " +s1 +" na "+s2);
+        System.out.println("5: dodanie we wskazanym miejscu ciagu"+ addThis);
+        System.out.println("6: sortowanie rosnaco");
+        System.out.println("7: sortowanie malejaco");
+        System.out.println("8: przeczytaj URL i policz wszystkie slowa i wszystkie litery");
+        System.out.println("0 aby wyjsc");
+        s = sc.nextInt();
 
+            switch (s) {
+                case 1: {
+                    count = fileReader.countWords(countThis);
+                    System.out.println(count);
+                    break;
+                }
+                case 2: {
+                    count = fileReader.countChars(countChar);
+                    System.out.println(count);
+                    break;
+                }
+                case 3: {
+                    fileReader.deleteStringToNewFile(deleteThis);
+                    System.out.println("zapisac do instniejacego pliku (1) czy do nowego pliku (2)");
+                    check1 = sc.nextInt();
+                    if (check1 == 1) {
+                        fileReader.deleteStringToSameFile(deleteThis);
+                    }
+                    break;
+                }
+                case 4: {
+                    fileReader.modify(s1, s2);
+
+                    break;
+                }
+                case 5: {
+                    System.out.println("Podaj w ktore miejsce (integer)");
+                    int place = sc.nextInt();
+                    fileReader.insertInto(addThis,place);
+                    break;
+                }
+                case 6: {
+                    fileReader.sortAsc();
+
+                    break;
+                }
+                case 7: {
+                    fileReader.sortDsc();
+                    break;
+                }
+                case 8: {
+                    fileReader.readURL();
+                    break;
+                }
+
+            }
+        }
     }
-}
